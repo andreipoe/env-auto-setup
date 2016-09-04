3f18da79e3d76683ce06da5a3ba3639360c5abcb
Andrei Poenaru <andrei.poenaru@gmail.com>
2016-09-04 14:38:39 +0100

    Implement place-files.

--- install.sh
+++ install.sh
@@ -89,6 +89,85 @@ function script () {
     GOODIES_DIR="$goodies_dir" eval "$cmd"
 }
 
+function place-files () {
+    local file="$1"
+
+    # Read the copy-specific options
+    if grep -q "^#TARGET overwrite" "$file"; then
+        local overwrite=yes
+    else
+        local overwrite=no
+    fi
+    if grep -q "^#TARGET append" "$file"; then
+        local append=yes
+    else
+        local append=no
+    fi
+    if grep -q "^#TARGET backup" "$file"; then
+        local backup=yes
+    else
+        local backup=no
+    fi
+
+    # Don't make a backup if neither appending nor overwriting
+    if [ "$overwrite" != "yes" -a "$append" != "yes" -a "$backup" == "yes" ]; then
+        echo "Ignoring #TARGET backup: neither overwrite nor append specified." >&2
+        backup=no
+    fi
+
+    while IFS='>' read -ra parts; do
+        [ -z "${parts[*]}" ] && continue # Skip empty lines
+        if [ "${#parts[*]}" != 2 ]; then
+            echo -n "Skipping improprely formatted line: ${parts[0]} " >&2
+            for p in ${parts[@]:1}; do
+                echo -n "> $p " >&2
+            done
+            echo >&2
+            continue
+        fi
+
+        # Parse the source
+        local source=$(echo ${parts[0]} | xargs) # Remove leading and trailing whitespace
+        source=$(readlink -m "$goodies_dir/$source") # Convert to full path
+
+        if [ ! -e "$source" ]; then
+            echo "Cannot find source file: $source." >&2
+            continue
+        fi
+
+        # Parse the destination
+        local dest=$(echo ${parts[1]} | xargs) # Remove leading and trailing whitespace
+        dest=$(readlink -m "$HOME/$dest") # Convert to full path
+
+        # Prepare the operations
+        local cmd=""
+        if [ -e "$dest" ]; then
+            if [ "$overwrite" == "yes" ]; then
+                [ "$backup" == "yes" ] && cmd="mv $dest $dest.bak && "
+                cmd="$cmd cp -a $source $dest"
+            elif [ "$append" == "yes" ]; then
+                [ "$backup" == "yes" ] && cmd="cp -a $dest $dest.bak && "
+                if [ -f "$source" -a -f "$dest" ]; then
+                    cmd="$cmd cat $source >> $dest"
+                else
+                    cmd="$cmd cp -a $source $dest"
+                fi
+            else
+                echo "Destination file already exists: $dest." >&2
+                echo "Use #TARGET overwrite or append if this is intended." >&2
+                continue
+            fi
+        else
+            cmd="$cmd cp -a $source $dest"
+        fi
+
+        # Copy the files
+        [ "$verbose" == "yes" ] && echo "$cmd"
+        eval "$cmd"
+
+    done < <(grep -v '^#' "$file")
+}
+
 # ------------------------
 
 target_dir="install-targets/"
@@ -238,6 +317,8 @@ if [ "${#selected_target_files[*]}" == 0 ]; then
     exit 0
 fi
 
+echo
+
 # Process targets
 [ "$verbose" == "yes" ] && echo_cmd="echo" || echo_cmd="echo -n"
 
@@ -267,12 +348,14 @@ for target_file in ${selected_target_files[@]}; do
 
     case "$type" in
         pkg-install)
-            # pkg-install "$target_file" # TODO: debugging
+            pkg-install "$target_file"
             ;;
         script)
             script "$target_file"
             ;;
-        # TODO: copy
+        place-files)
+            place-files "$target_file"
+            ;;
     esac
 
     if [ "$?" == 0 -a "$verbose" != "yes" ]; then
